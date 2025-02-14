27f82a9eb4ce964e15dcd481a9259adb1478f653
nguyen.doanvo <nguyen.doanvo@ncc.asia>
2025-02-14 11:18:37 +0700

    fix: change mysql config to host

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -5,10 +5,10 @@ import 'dotenv/config';
 // MySQL DataSource
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
+    host: process.env.MYSQL_PRODUCTION_HOST,
+    port: parseInt(process.env.MYSQL_PRODUCTION_PORT, 10),
+    username: process.env.MYSQL_PRODUCTION_USERNAME,
+    password: process.env.AVNS_bsK6jN7r1n481OEv,
     database: "teaching",
     synchronize: true,
     logging: false,
