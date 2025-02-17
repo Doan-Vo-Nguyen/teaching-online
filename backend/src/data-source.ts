2f3d6bab77dfc4df9588424a325a12609301f260
nguyen.doanvo <nguyen.doanvo@ncc.asia>
2025-02-17 15:44:42 +0700

    fix: fix logic validate

---
+++ backend/src/data-source.ts
@@ -0,0 +1,34 @@
+import { DataSource } from "typeorm";
+import "reflect-metadata";
+import 'dotenv/config';
+
+// MySQL DataSource
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.MYSQL_PRODUCTION_HOST,
+    port: parseInt(process.env.MYSQL_PRODUCTION_PORT, 10),
+    username: process.env.MYSQL_PRODUCTION_USERNAME,
+    password: process.env.MYSQL_PRODUCTION_PASSWORD,
+    database: process.env.MYSQL_PRODUCTION_DB_NAME,
+    synchronize: true,
+    logging: false,
+    entities: ['src/**/entity/*.entity.{ts,js}'],
+    migrations: [],
+    subscribers: [],
+});
+
+// MongoDB DataSource
+export const AppDataSource2 = new DataSource({
+    type: "mongodb",
+    host: "localhost",
+    port: 27017, 
+    database: "teaching_online",
+    username: process.env.MONGO_USERNAME,
+    password: process.env.MONGO_PASSWORD, 
+    useUnifiedTopology: true,
+    synchronize: true,
+    logging: true,
+    entities: ['src/**/entity/Comment.mongo.ts'],
+    migrations: [],
+    subscribers: [],
+});
