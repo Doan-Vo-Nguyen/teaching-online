8d94b2995e05c1129d1c9298b5167f82cb121c65
nguyen.doanvo <nguyen.doanvo@ncc.asia>
2025-02-18 14:04:45 +0700

    feat: add lectures entity

--- backend/src/modules/middleware/auth.middleware.ts
+++ backend/src/modules/middleware/auth.middleware.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import { Response, NextFunction } from 'express';
 import { IRequest } from '../types/IRequest';
-import { AUTHEN_ERROR, INVALID_TOKEN } from '../DTO/resDto/BaseErrorDto';
+import { AUTHENTICATION_ERROR, INVALID_TOKEN } from '../DTO/resDto/BaseErrorDto';
 import jwt from 'jsonwebtoken';
 import { Logger } from '../config/logger';
 dotenv.config();
@@ -10,7 +10,7 @@ const JWT_KEY = process.env.JWT_KEY;
 
 export const authentication = async (req: IRequest, res: Response, next: NextFunction) => {
     if (!req.headers['authorization']) {
-        return res.status(401).json({AUTHEN_ERROR});
+        return res.status(401).json({AUTHENTICATION_ERROR});
     }
 
     const token = req.headers['authorization'].split(' ')[1];
@@ -30,7 +30,7 @@ const author = (ROLE_TYPES) => {
         const {role} = req.user;
         try {
             if (!role.some((r) => ROLE_TYPES.includes(r))) {
-                return res.status(403).json({AUTHEN_ERROR});
+                return res.status(403).json({AUTHENTICATION_ERROR});
             }
             next();
         }
@@ -46,7 +46,7 @@ export const authorAdmin = (req: IRequest, res: Response, next: NextFunction) =>
 
     try {
         if (!role.includes('admin')) {
-            return res.status(403).json({AUTHEN_ERROR});
+            return res.status(403).json({AUTHENTICATION_ERROR});
         }
         next();
     } catch (error) {
