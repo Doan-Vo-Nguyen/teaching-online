df615b796b6f02fb6a031af4bcc8e6ece3137e7e
nguyen.doanvo <nguyen.doanvo@ncc.asia>
2025-02-24 14:18:24 +0700

    fix: change to localhost

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -5,11 +5,11 @@ import 'dotenv/config';
 // MySQL DataSource
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: process.env.MYSQL_PRODUCTION_HOST,
-    port: parseInt(process.env.MYSQL_PRODUCTION_PORT, 10),
-    username: process.env.MYSQL_PRODUCTION_USERNAME,
-    password: process.env.MYSQL_PRODUCTION_PASSWORD,
-    database: process.env.MYSQL_PRODUCTION_DB_NAME,
+    host: "localhost",
+    port: 3306,
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: "teaching",
     synchronize: true,
     logging: false,
     entities: ['src/**/entity/*.entity.{ts,js}'],
