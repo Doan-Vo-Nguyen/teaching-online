2f89b1a0e85410dac3bd334474f2cd5c8a312965
nguyen.doanvo <nguyen.doanvo@ncc.asia>
2025-02-26 16:18:57 +0700

    change port for product

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -110,7 +110,7 @@ export class Application {
   }
 
   public async start() {
-    const port = process.env.APP_PORT || 3000;
+    const port = process.env.PORT || 10000;
     const name = process.env.APP_SERVER || "Teaching_Online_Server";
     try {
       await AppDataSource.initialize();
