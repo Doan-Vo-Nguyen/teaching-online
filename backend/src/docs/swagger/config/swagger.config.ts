73fae27d0d4dc16a6c0ddd5f70f044f90fce1521
nguyen.doanvo <nguyen.doanvo@ncc.asia>
2025-03-01 12:30:02 +0700

    chore: change to host

--- backend/src/docs/swagger/config/swagger.config.ts
+++ backend/src/docs/swagger/config/swagger.config.ts
@@ -9,7 +9,7 @@ const options = {
     },
     servers: [
       {
-        url: process.env.SWAGGER_URL || "https://localhost:3000",
+        url: process.env.SWAGGER_PRODUCTION_URL || "https://teaching-online-server.onrender.com/",
         description: "Development server",
       },
     ],
